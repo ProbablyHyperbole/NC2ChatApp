79a77f970f793520e8c0879684034c7ea4558c1b
ProbablyHyperbole <kdp270@msstate.edu>
2017-05-10 14:39:34 -0500

    added connection handling

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const path = require('path'); //Native
+const http = require('http');
 const express = require('express'); //NonNative
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
-
+var server = http.createServer(app);
 app.use(express.static(publicPath));
+var io = socketIO(server);
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New User Connected');
+
+  socket.on('disconnect', () => {
+    console.log('User Disconnected');
+  });
+
+});
+
+
+server.listen(port, () => {
 console.log(`Server is up on ${port}`);
 });
