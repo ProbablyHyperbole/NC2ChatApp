9f57ed52fc5b608501e79d8e537b896c3e91e286
ProbablyHyperbole <kdp270@msstate.edu>
2017-05-10 13:28:18 -0500

    Removed space between || causing port failure

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path'); //Native
 const express = require('express'); //NonNative
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT | | 3000;
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(publicPath));
